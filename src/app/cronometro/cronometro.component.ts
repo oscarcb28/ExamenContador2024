9933e6110884af9e5c4e6257cdee65808c663eb3
oscar <oscarcapinbouzan@gmail.com>
2025-04-02 18:13:47 +0200

    .

--- src/app/cronometro/cronometro.component.ts
+++ src/app/cronometro/cronometro.component.ts
@@ -7,31 +7,48 @@ import { Router } from '@angular/router';
   styleUrl: './cronometro.component.css'
 })
 export class CronometroComponent {
+clicked: any;
 
   constructor(private router: Router) {}
 
-  numero:number = 0;
+  
+  segundos:number = 0;
+  minutos:number = 0;
+  horas:number = 0;
 
   listaTiempos:[]=[]
 
   play(): void {
     this.cuenta();
-  }
-
-  stop(): void {
     
   }
 
-  async cuenta(){
-    while(true){
-      this.numero++;      
-      await this.esperar(1000);
-    }
+  stop(): void {
+    this.cuenta();
   }
 
-  record(){
-    this.numero.toString
+  async cuenta(){
+    while(true){   
+      this.segundos++;
+      if (this.segundos == 60){
+        this.segundos = 0;
+        this.minutos++;
+        if (this.minutos == 60){
+          this.minutos = 0;
+          this.horas++;
+        }
+      }      
+      await this.esperar(1000);               
+    }
   }
+  
+  record(){
+    this.segundos.toString
+    this.minutos.toString
+    this.horas.toString
+  }
+
+  
 
   esperar(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
